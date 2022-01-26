ce951ca130595d74ec14eb67b6ee9a551e802189
deniskapel <8568400+deniskapel@users.noreply.github.com>
2022-01-26 09:34:57 +0300

    Add catboost

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,3 +1,48 @@
+from typing import List, Tuple
+
+from sklearn.preprocessing import MultiLabelBinarizer
+
+Labels = List[Tuple[str, str]]
+EncodedLabels = List[List[int]]
+
+class LabelEncoder:
+    
+    """ 
+    Returns encoded labels in the following formats:
+    1. Midas labels only
+    2. Entity labels only
+    3. Their concatenation
+    4. Multilabeled binarization.
+    
+    First three
+
+    The first two will be used when a separate classifier is applied
+    while their concatenation is used with a universal classifier.
+    Multilabeled binarization is based on sklearn MultiLabelBinarizer
+    """
+    def __init__(self, classes: list, encoding: str='multi'):
+        self.mlb = MultiLabelBinarizer()
+        self.mlb.classes = classes
+        self.encoding = self.__validate_encoding(encoding)
+    
+    def to_categorical(self, labels: Labels) -> EncodedLabels:
+        """ encodes labels for tensorflow models """
+        if self.encoding == 'midas':
+            labels = [sample[:1] for sample in labels]
+        elif self.encoding == 'entities':
+            labels = [sample[1:] for sample in labels]
+        elif self.encoding == 'concatenation':
+            labels = [["_".join(sample)] for sample in labels]
+            
+        return self.mlb.fit_transform(labels)
+        
+    def __validate_encoding(self, encoding: str) -> str:
+        # validates encoding type
+        err_message = "Sorry, choose one of those: 'midas', 'entity', 'concatenation', 'multi'"
+        assert encoding in ['midas', 'entity', 'concatenation', 'multi'], err_message
+        return encoding
+    
+    
 def dummy_fn(doc):
     """ dummy function to apply tfidf to pre-tokenized docs """
     return doc
